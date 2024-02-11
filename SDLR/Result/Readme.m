eb30476586a0b200f6101ac5551fb19be15cbbf6
DRAGON23 <81781010+DRAGON20-3@users.noreply.github.com>
2024-02-11 11:27:36 +0330

    Update Readme.m

--- SDLR/Result/Readme.m
+++ SDLR/Result/Readme.m
@@ -1,1 +1,1 @@
-The result of model and t-trst of mrdel are here
+Results include of <b>NDCG</b> and <b>MRR</b> metircs are computed for baseline methods and our proposed method (SDLR) that are in `Baselines` and `SDLR` respectfully.
